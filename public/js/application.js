db73b0143d4ef579936ec9c76b28ef0b0857b4e2
Randy Grewal <rgrewalucsb@gmail.com>
2015-02-24 17:16:58 -0800

    test

---
+++ public/js/application.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+  $('.panel-heading a').on('click', function(event){
+  	$('.panel-body').removeClass('active')
+  	$(this).parent().parent().find('.panel-body').addClass('active')
+  	$(this).parent().parent().find('.panel-body').addClass("animated fadeIn");
+  })
+
+  $('a.navname').on('click', function(){
+  	$parent = $('a.navname').parent().removeClass('active')
+  	$(this).parent().addClass('active')
+  	var $li = $(this).parent();
+  	if ($li.attr('id') == 'home'){
+  		$("[id$=_main").css('display','none');
+  		$("#index_main").css('display', 'block');
+  	}
+  	if ($li.attr('id') == 'resume'){
+  		$("[id$=_main").css('display','none');
+  		$("#resume_main").css('display', 'block');
+  	}
+  	if ($li.attr('id') == 'blog'){
+  		$("[id$=_main").css('display','none');
+  		$("#blog_main").css('display', 'block');
+  	}
+  	if ($li.attr('id') == 'projects'){
+  		$("[id$=_main").css('display','none');
+  		$("#projects_main").css('display', 'block');
+  	}
+  })
+
+});
